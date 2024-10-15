a9f058b37840029644483bf56671886e58258ed2
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-10-15 17:27:53 +0200

    feat: added an HTTP server to use socket.io

--- server/src/main.js
+++ server/src/main.js
@@ -1,33 +1,54 @@
 // dotenv INIT
 require("dotenv").config();
 
+// Socket.io INIT
+const { Server } = require('socket.io');
+
 // app INIT
 const cookieParser = require("cookie-parser");
 const express = require('express');
+const http = require('http'); // Import du module http
 const pairOuImpairRouter = require('./routes/pairOuImpair.js');
 const loginRouter = require('./routes/login.js');
 const logoutRouter = require('./routes/logout.js');
 
-// app server INIT
-const server = express();
+// Crée une application Express
+const app = express();
 const port = 3000;
 
-// use express-json and cookieParser
-server.use(express.json());
-server.use(cookieParser());
+// Crée un serveur HTTP à partir de l'application Express
+const server = http.createServer(app);
+
+// Attache Socket.io au serveur HTTP
+const io = new Server(server);
+
+// Middleware pour traiter les JSON et les cookies
+app.use(express.json());
+app.use(cookieParser());
 
 // Routes INIT
-server.get('/', (req, res) => {
-    res.send("Hello World")
+app.get('/', (req, res) => {
+    res.send("Hello World");
 });
 
-server.use(pairOuImpairRouter);
-server.use(loginRouter);
-server.use(logoutRouter);
+app.use(pairOuImpairRouter);
+app.use(loginRouter);
+app.use(logoutRouter);
 
-// Socket.io
+// Gestion des événements Socket.io
+io.on("connection", (socket) => {
+    console.log('Un utilisateur est connecté');
+    
+    socket.on('message', (msg) => {
+        console.log('Message reçu :', msg);
+    });
 
+    socket.on('disconnect', () => {
+        console.log('Un utilisateur est déconnecté');
+    });
+});
 
+// Démarre le serveur HTTP sur le port spécifié
 server.listen(port, () => {
     console.log(`API listening on http://localhost:${port}`);
-})
+});
