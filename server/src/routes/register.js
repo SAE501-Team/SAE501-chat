66ed7f15642075eec246c036dad5118d5e0cfeaf
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-10-26 19:00:41 +0200

    feat: added postman collection and mysql database dump

--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const database = require('../utils/db/databaseInit.js');
+const database = require("../utils/db/databaseInit.js");
 
 /*
     Enregistre un utilisateur dans la base de donnée
@@ -12,24 +12,27 @@ const database = require('../utils/db/databaseInit.js');
         username (prestashop)
     }
 */
-router.post('/register', async (req, res) => {
-    const { id, username } = req.body;
+router.post("/register", async (req, res) => {
+  const { id, username, email } = req.body;
 
-    try {
-        // Enregistre l'utilisateur dans la bdd du chat
-        await database.query('INSERT INTO user (id, username) VALUES (?, ?)', [id, username]);
+  try {
+    // Enregistre l'utilisateur dans la bdd du chat
+    await database.query(
+      "INSERT INTO user (id, username, email) VALUES (?, ?, ?)",
+      [id, username, email]
+    );
 
-        return res.json({
-            success: true,
-            message: `Félicitations ${username}, vous êtes enregistré !`
-        });
-    } catch (error) {
-        console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', error);
-        return res.status(500).json({
-            success: false,
-            message: 'Erreur lors de l\'enregistrement de l\'utilisateur.'
-        });
-    }
+    return res.json({
+      success: true,
+      message: `Félicitations ${username}, vous êtes enregistré !`,
+    });
+  } catch (error) {
+    console.error("Erreur lors de l'enregistrement de l'utilisateur :", error);
+    return res.status(500).json({
+      success: false,
+      message: "Erreur lors de l'enregistrement de l'utilisateur.",
+    });
+  }
 });
 
 module.exports = router;
