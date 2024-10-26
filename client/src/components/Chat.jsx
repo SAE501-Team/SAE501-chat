74c80dea5b4bdc60d06742f4d58a9b9364a5ff47
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-10-26 18:02:10 +0200

    fix: fixed node.js CORS error

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,38 +1,62 @@
-import React from 'react';
-import '../Chat.css';
-import io from 'socket.io-client';
+import { useEffect, useState } from "react";
+import "../Chat.css";
+import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:3000");
+
 const Chat = () => {
-    const sendMessage = () => {
-        socket.emit();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Listener pour les nouveaux messages
+    socket.on("message", (newMessage) => {
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    });
+
+    // Clean up on unmount
+    return () => {
+      socket.off("message");
     };
+  }, []);
 
-    return (
-        <div className="chat-w">
-            <div className="chat-goat">
-                <img src="/goat1.png" alt="" />
+  const sendMessage = () => {
+    socket.emit("message", message);
+    setMessage(""); // Réinitialise le champ
+  };
 
-                <div className="goat2">
-                    <img src="/goat2.png" alt="" />
-                </div>
-            </div>
+  return (
+    <div className="chat-w">
+      <div className="chat-goat">
+        <img src="/goat1.png" alt="" />
 
-            <div className="chat-show">
-
-                <div className="chat-area">
-                    <div className="chat-text"></div>
-                </div>
-
-                <div className="chat-br">
-                    <input type="text" className="chat-in" placeholder="Type something..." />
-                    <button className="chat-send" onClick={sendMessage}>
-                        <img src="chat-submit.svg" alt="Send" />
-                    </button>
-                </div>
-            </div>
+        <div className="goat2">
+          <img src="/goat2.png" alt="" />
         </div>
-    );
+      </div>
+
+      <div className="chat-show">
+        <div className="chat-area">
+          <div className="chat-text">
+            {messages.map((msg, index) => (
+              <p key={index}>{msg}</p>
+            ))}
+          </div>
+        </div>
+
+        <div className="chat-br">
+          <input
+            type="text"
+            className="chat-in"
+            placeholder="Type something..."
+          />
+          <button className="chat-send" onClick={sendMessage}>
+            <img src="chat-submit.svg" alt="Send" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
